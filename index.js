19b28d6907e706c266e31e91f78d207890adc118
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-04-22 10:37:31 -0500

    initial commit started setup

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+const server = express();
+const PORT = '9001';
+
+
+server.use(express.json());
